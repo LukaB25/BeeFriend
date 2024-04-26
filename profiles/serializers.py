0d38bb4256e6e04f81ee9f0a5db2c1b521f0d126
Luka Black <luka.black20@gmail.com>
2024-04-26 14:20:29 +0000

    add in-browser login, logout, permissions and is_owner field to ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,12 +4,18 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id',
             'owner',
+            'is_owner',
             'name',
             'bio',
             'image',
