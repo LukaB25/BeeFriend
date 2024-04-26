f848cde1fe930981a2b72ba82765db870ba65374
Luka Black <luka.black20@gmail.com>
2024-04-26 15:32:28 +0000

    Add PostList view to posts app, corrected serializer errors, add posts to settings.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,21 +6,22 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ImageField(source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
         if value.size > 1024 *1024 *2:
             raise serializers.ValidationError(
                 'Image size is too big, it must be less than 2MB'
             )
-        if value.width > 1920:
+        if value.image.width > 1920:
             raise serializers.ValidationError(
                 'Image width is too large, it must be less than 1920px'
             )
-        if value.height > 1920:
+        if value.image.height > 1920:
             raise serializers.ValidationError(
                 'Image height is too large, it must be less than 1920px'
             )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
