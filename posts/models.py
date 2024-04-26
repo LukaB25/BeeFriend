6b32dc1a78d7551e220e0d5bdb08e047ed465e7f
Luka Black <luka.black20@gmail.com>
2024-04-26 15:07:21 +0000

    add posts app, post model and serializers with image size validator

---
+++ posts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    """
+    Post model related to the User instance
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/',
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+    
+    def __str__(self):
+        return f"{self.owner.username} posted {self.title}"
--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,10 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
+    """
+    Profile model related to the User instance
+    Default image is returned if no image is uploaded
+    """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=True)
     bio = models.TextField(blank=True)
