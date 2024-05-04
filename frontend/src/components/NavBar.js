c4773bb890424aa56f2d95c0d5a2ae7cd44001fd
Luka Black <luka.black20@gmail.com>
2024-05-04 17:09:56 +0000

    feat: add toggle navbar dropdown menu hook to handle click outside it.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,11 +8,14 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
   const handleLogout = async () => {
     try {
@@ -61,7 +64,7 @@ const NavBar = () => {
   )
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand
@@ -69,7 +72,11 @@ const NavBar = () => {
               BEE<Icon icon="gg:bee" className={styles.LogoIcon} />FRIEND
           </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+          className={styles.Toggle} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
